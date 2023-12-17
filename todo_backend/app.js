5bb6f28a3aa367e82b3e3af84cee27154ca033b9
zyd <zydmayday@gmail.com>
2023-12-17 22:17:12 +0900

    add htmx, button to add todo

--- todo_backend/app.js
+++ todo_backend/app.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // 存储todos的数组
@@ -11,24 +13,25 @@ let todos = [];
 let todoIdCounter = 1;
 
 // 获取所有Todos
-app.get('/todos', (req, res) => {
+app.get("/todos", (req, res) => {
   res.json({ todos });
 });
 
 // 获取特定ID的Todo
-app.get('/todos/:id', (req, res) => {
+app.get("/todos/:id", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
-  const todo = todos.find(todo => todo.id === todoId);
+  const todo = todos.find((todo) => todo.id === todoId);
 
   if (todo) {
     res.json({ todo });
   } else {
-    res.status(404).json({ message: 'Todo not found' });
+    res.status(404).json({ message: "Todo not found" });
   }
 });
 
 // 添加Todo
-app.post('/todos', (req, res) => {
+app.post("/todos", (req, res) => {
+  console.log("add new todos", req.body);
   const todoText = req.body.text;
 
   if (todoText) {
@@ -38,36 +41,48 @@ app.post('/todos', (req, res) => {
     };
 
     todos.push(newTodo);
-    res.status(201).json({ success: true, message: 'Todo added successfully', todo: newTodo });
+    res.status(201).json({
+      success: true,
+      message: "Todo added successfully",
+      todo: newTodo,
+    });
   } else {
-    res.status(400).json({ success: false, message: 'Todo text is required' });
+    res.status(400).json({ success: false, message: "Todo text is required" });
   }
 });
 
 // 更新Todo
-app.put('/todos/:id', (req, res) => {
+app.put("/todos/:id", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
-  const index = todos.findIndex(todo => todo.id === todoId);
+  const index = todos.findIndex((todo) => todo.id === todoId);
 
   if (index !== -1) {
     const updatedTodo = { id: todoId, text: req.body.text };
     todos[index] = updatedTodo;
-    res.json({ success: true, message: 'Todo updated successfully', todo: updatedTodo });
+    res.json({
+      success: true,
+      message: "Todo updated successfully",
+      todo: updatedTodo,
+    });
   } else {
-    res.status(404).json({ success: false, message: 'Todo not found' });
+    res.status(404).json({ success: false, message: "Todo not found" });
   }
 });
 
 // 删除Todo
-app.delete('/todos/:id', (req, res) => {
+app.delete("/todos/:id", (req, res) => {
   const todoId = parseInt(req.params.id, 10);
-  const index = todos.findIndex(todo => todo.id === todoId);
+  const index = todos.findIndex((todo) => todo.id === todoId);
 
   if (index !== -1) {
     const deletedTodo = todos.splice(index, 1);
-    res.json({ success: true, message: 'Todo deleted successfully', deletedTodo });
+    res.json({
+      success: true,
+      message: "Todo deleted successfully",
+      deletedTodo,
+    });
   } else {
-    res.status(404).json({ success: false, message: 'Todo not found' });
+    res.status(404).json({ success: false, message: "Todo not found" });
   }
 });
 
